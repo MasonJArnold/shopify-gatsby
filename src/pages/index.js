9a373195fe2f5d4175d98594d1f2e73f1709c454
MasonJArnold <mason.jarnold@gmail.com>
2021-09-15 11:15:38 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
-import 'reset-css'
+// import 'reset-css'
 
 const IndexPage = () => {
 	
