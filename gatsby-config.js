02166ad6678e8ac9818544b63d2452a183e79139
MasonJArnold <mason.jarnold@gmail.com>
2021-09-15 10:52:30 -0400

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,16 @@
 // require('dotenv').config()
 const path = require('path')
-
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
-    title: `change me`,
+    title: `shopless`,
     description: `change me`,
-    author: `your info here`,
+    author: `vanish`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+   `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -36,18 +38,19 @@ module.exports = {
         policy: [{ userAgent: '*', allow: ['/'] }]
       }
     },
-    {
-      resolve: `gatsby-plugin-transition-link`,
-      options: {
-        layout: require.resolve(`./src/layouts/index.js`)
-      }
-    },
+    // {
+    //   resolve: `gatsby-plugin-transition-link`,
+    //   options: {
+    //     layout: require.resolve(`./src/layouts/index.js`)
+    //   }
+    // },
     {
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_DATASET,
-        token: process.env.SANITY_API_TOKEN
+        projectId: '98iq9hxm', //process.env.SANITY_PROJECT_ID,
+        dataset: 'production', //process.env.SANITY_DATASET,
+        token: 'skjQ08BmnLkpT5q4HKrpktA6ctGaL3zxRIgsP4qzExm0cSSIk4wsGQ8nqvaEsJLR5Np7F4NcYaFNEJEfOMlGs8xewdy1YjuCIHkTw0raRB0esBlVjAVwWLWNofFnhlpE0DDf7BMkxMI53NhTiRCj900s6E9KYOGnWLyH36FktbZvqUMy1O03',//process.env.SANITY_API_TOKEN,
+        graphqlTag: 'default',
       }
     },
 
@@ -64,20 +67,20 @@ module.exports = {
     },
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [
-          require(`postcss-preset-env`)({ stage: 0 }),
-          require('postcss-import'),
-          require('postcss-nested'),
-          require('postcss-cssnext'),
-          require('postcss-calc'),
-          require('postcss-discard-comments'),
-          require('postcss-reporter')
-        ]
-      }
-    },
+    // {
+    //   resolve: `gatsby-plugin-postcss`,
+    //   options: {
+    //     postCssPlugins: [
+    //       require(`postcss-preset-env`)({ stage: 0 }),
+    //       require('postcss-import'),
+    //       require('postcss-nested'),
+    //       require('postcss-cssnext'),
+    //       require('postcss-calc'),
+    //       require('postcss-discard-comments'),
+    //       require('postcss-reporter')
+    //     ]
+    //   }
+    // },
   
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
