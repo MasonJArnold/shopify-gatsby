ea95e29bcee0d10b9cf7bcc88881126ff56d9c32
masonvanish <mason@wearevanish.com>
2021-09-15 09:38:07 -0400

    add missing src

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/Layout/Layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const About = () => {
+
+  return ( 
+    <Layout>
+      <main className='about'>
+        This Explains how the repo is supposed to work and HELP!?
+      </main>
+    </Layout>
+  )
+  }
+
+export default About
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+import SEO from "../components/seo"
+import 'reset-css'
+
+const IndexPage = () => {
+	
+	return (
+		<Layout>
+			<SEO title="Home" />
+			tight
+		</Layout>
+	  )
+}
+
+export default IndexPage
