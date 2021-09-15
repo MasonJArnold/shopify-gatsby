5c8788cd3820d59090dbfc0c9ffb0edfbc6da0b9
MasonJArnold <mason.jarnold@gmail.com>
2021-09-15 11:19:35 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,12 @@ module.exports = {
         icon: `src/images/favicon/apple-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        policy: [{ userAgent: '*', allow: ['/'] }]
-      }
-    },
+//     {
+//       resolve: 'gatsby-plugin-robots-txt',
+//       options: {
+//         policy: [{ userAgent: '*', allow: ['/'] }]
+//       }
+//     },
     // {
     //   resolve: `gatsby-plugin-transition-link`,
     //   options: {
